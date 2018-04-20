74c6a84eb0a914d3f5d455b7b8f22c307b5bc56f
intellhave <intellhave@gmail.com>
2018-04-20 16:22:01 +1000

    Changed Binary solver - faster

--- binary_solver_new.py
+++ binary_solver_new.py
@@ -6,6 +6,69 @@ from scipy.optimize import minimize
 from scipy.linalg import norm
 from autograd import grad
 #from ad import gh
+def BinarySolver_v1(func, x0, rho, maxIter):
+    """
+    Use exact penalty method to solve optimization problem with binary constraints
+        min_x func(x)
+        s.t. x \in {-1,1}
+
+    Inputs:
+        - func: Function that needs to be minimized
+        - x0:   Initialization
+    Refereces: https://www.facebook.com/dangkhoasdc
+
+    """
+    
+    n = len(x0)  
+    #xt, vt: Values of x and v at the previous iteration, which are used to update x and v at the current iteration, respectively
+    xt = x0
+    vt = np.zeros(xt.shape)  # Initialize v to zeros!!!!!!! Note on this
+
+
+    def fx(x): # Fix v, solve for x
+        return func(x) + rho*(np.dot(x,vt))
+
+    def fv(x): # Fix x, solve for v
+        return np.dot(xt, x)
+
+    # Define the lower and upper bounds for fx, i.e., -1 <= x <= 1
+    xBounds = [[-1,1] for i in range(n)]
+    
+    # Ball-constraint ||v||^2 <= n
+    vConstraints = ({'type':'ineq',
+            'fun': lambda x: np.array([n - norm(x)**2]),
+            'jac': lambda x: np.array(-2*x)
+           })
+
+    # Now, let the iterations begin
+    converged = False
+    iter = 0
+    while iter < maxIter and not converged:               
+        # Fix v, minimize x
+        print('----Update x steps')
+        x_res = minimize(fx, xt, bounds = xBounds, tol=1e-3, method = 'Newton-CG')
+        x = x_res.x
+
+        # Fix x, update v
+        print('----Update v steps')
+        v_res = minimize(fv, vt, constraints = vConstraints, method = 'COBYLA')
+        v = v_res.x
+
+        # Check for convergence
+        if iter > 5 and (norm(x - xt) < 1e-9 or (func(x) - func(xt) < 1e-9)):
+            converged = True
+            print('--------Converged---------')
+            return x
+
+        print("Iter: %d , cost: %f" %(iter, func(xt)))
+        #print (xt)
+        rho = rho*1.1
+        xt = x
+        vt = v
+        iter = iter + 1
+
+    return xt
+
 
 ########################################################################
 def BinarySolver(func, x0, rho, maxIter):
@@ -54,71 +117,6 @@ def BinarySolver(func, x0, rho, maxIter):
         print('----Update x steps')        
         #x_res = minimize(fx, xt, bounds = xBounds, method='SLSQP',jac = gradx)
         x_res = minimize(fx, xt, constraints = xConstraints, method='COBYLA')
-        x = x_res.x
-
-        # Fix x, update v
-        print('----Update v steps')
-        v_res = minimize(fv, vt, constraints = vConstraints, method = 'COBYLA')
-        v = v_res.x
-
-        # Check for convergence
-        if iter > 5 and (norm(x - xt) < 1e-9 or (func(x) - func(xt) < 1e-9)):
-            converged = True
-            print('--------Converged---------')
-            return x
-
-        print("Iter: %d , cost: %f" %(iter, func(xt)))
-        #print (xt)
-        rho = rho*1.1
-        xt = x
-        vt = v
-        iter = iter + 1
-
-    return xt
-
-
-#================================================================================
-def BinarySolver_v1(func, x0, rho, maxIter):
-    """
-    Use exact penalty method to solve optimization problem with binary constraints
-        min_x func(x)
-        s.t. x \in {-1,1}
-
-    Inputs:
-        - func: Function that needs to be minimized
-        - x0:   Initialization
-    Refereces: https://www.facebook.com/dangkhoasdc
-
-    """
-    
-    n = len(x0)  
-    #xt, vt: Values of x and v at the previous iteration, which are used to update x and v at the current iteration, respectively
-    xt = x0
-    vt = np.zeros(xt.shape)  # Initialize v to zeros!!!!!!! Note on this
-
-
-    def fx(x): # Fix v, solve for x
-        return func(x) + rho*(np.dot(x,vt))
-
-    def fv(x): # Fix x, solve for v
-        return np.dot(xt, x)
-
-    # Define the lower and upper bounds for fx, i.e., -1 <= x <= 1
-    xBounds = [[-1,1] for i in range(n)]
-    
-    # Ball-constraint ||v||^2 <= n
-    vConstraints = ({'type':'ineq',
-            'fun': lambda x: np.array([n - norm(x)**2]),
-            'jac': lambda x: np.array(-2*x)
-           })
-
-    # Now, let the iterations begin
-    converged = False
-    iter = 0
-    while iter < maxIter and not converged:               
-        # Fix v, minimize x
-        print('----Update x steps')
-        x_res = minimize(fx, xt, bounds = xBounds, tol=1e-3, method = 'Newton-CG')
         x = x_res.x
 
         # Fix x, update v
--- test_binary_solver.py
+++ test_binary_solver.py
@@ -9,14 +9,14 @@ from binary_solver import BinarySolver, BruteForceBinarySolver, NextPermute
 
 # Test with a random function min x'Wx
 
-x0 = -1*np.ones(100)
+x0 = -1*np.ones(1000)
 dim = len(x0)
 W = np.random.rand(dim, dim)
 b = np.random.rand(dim, 1)
 
 def myFunction(x):
     xx  = x[None, :]
-    return xx.dot(W).dot(xx.T)[0][0] + xx.dot(b)
+    return np.squeeze(xx.dot(W).dot(xx.T)) + xx.dot(b)
 
 # sol, minCost = BruteForceBinarySolver(myFunction, x0)
 # print('------BRUTEFORCE SOLUTION--------')
@@ -26,7 +26,7 @@ def myFunction(x):
 
 
 print('------EXACT PENALTY--------')
-rho = 10
+rho = 0.1
 maxIter =  1000
 x = BinarySolver(myFunction, x0, rho, maxIter)
 print(x)
